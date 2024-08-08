6dd32fb5d38e1d3d5a65690293e899ae87e784b3
Gogs <gogs@fake.local>
2024-08-08 16:51:44 +0600

    Intial design done

--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
